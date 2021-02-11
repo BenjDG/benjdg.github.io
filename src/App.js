648a0438e30658c764faf1f0971fe9e21d24be58
Ben Galloway <bdgalloway85@gmail.com>
2021-02-10 21:13:24 -0600

    remove theme code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { ThemeProvider } from '@material-ui/core/styles';
 import Home from './pages/Home';
 
-const theme = {
-};
 
 function App () {
   return (
     <div>
-      <ThemeProvider theme={theme}>
         <Home />
-      </ThemeProvider>
     </div>
   );
 }
