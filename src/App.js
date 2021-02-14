3238683e172dd270a34d3e5224c6aa5fcbe4e4c9
Ben Galloway <bdgalloway85@gmail.com>
2021-02-14 01:10:29 -0600

    fix 3

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import Home from './pages/Home';
 
 =======
 import React from "react";
-import { ThemeProvider } from '@material-ui/core/styles';
 import Home from './pages/Home';
 
-const theme = {
-};
 
 >>>>>>> 0bf3d99... add new picture
 function App () {
   return (
     <div>
-      <ThemeProvider theme={theme}>
         <Home />
-      </ThemeProvider>
     </div>
   );
 }
