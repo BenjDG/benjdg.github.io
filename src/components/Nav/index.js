7e4e116d6b7c115bdd82fddc82a2d85532e39202
Ben Galloway <bdgalloway85@gmail.com>
2021-02-14 01:11:26 -0600

    fix 5

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react';
 
 function Nav () {
@@ -8,6 +9,10 @@ function Nav () {
 =======
 import React from "react";
 import { AppBar, IconButton, makeStyles, Toolbar, Typography } from "@material-ui/core";
+=======
+import React, { useEffect, useState } from "react";
+import { AppBar, ClickAwayListener, IconButton, makeStyles, Menu, MenuItem, Toolbar, Typography } from "@material-ui/core";
+>>>>>>> 86143f1... add hamburger button
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,10 +25,50 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  typographyStyle: {
+    flex: 1,
+  }
 }));
 
 function Nav () {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [location, setLocation] = useState('');
   const classes = useStyles();
+
+  useEffect(() => {
+    switch (location) {
+      case "Portfolio":
+        document.querySelector('#portfolio').scrollIntoView();
+        break;
+      case "About Me":
+        document.querySelector('#about-me').scrollIntoView();
+        break;
+      case "Contact":
+        document.querySelector('#contact').scrollIntoView();
+        break;
+
+      default:
+        break;
+    }
+  }, [location]);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+
+  };
+
+  const handleClose = (event) => {
+    if (event.nativeEvent.target.firstChild) {
+      setLocation(event.nativeEvent.target.firstChild.data);
+      setAnchorEl(null);
+    }
+  };
+
+  const handleClickAway = () => {
+    setLocation('');
+    setAnchorEl(null);
+  };
+
   return (
 <<<<<<< HEAD
     <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
@@ -37,6 +82,7 @@ function Nav () {
     <div>
       <AppBar position='static'>
         <Toolbar>
+<<<<<<< HEAD
           <Typography>Ben Galloway</Typography>
 
 >>>>>>> 84bc03b... add AppBar
@@ -67,6 +113,26 @@ function Nav () {
       <MenuIcon />
     </IconButton>
       </Toolbar>
+=======
+          <Typography className={classes.typographyStyle}>Ben Galloway</Typography>
+          <ClickAwayListener onClickAway={handleClickAway}>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" aria-haspopup="true" onClick={handleClick}>
+              <MenuIcon />
+            </IconButton>
+          </ClickAwayListener>
+          <Menu
+            id="menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>About Me</MenuItem>
+            <MenuItem onClick={handleClose}>Portfolio</MenuItem>
+            <MenuItem onClick={handleClose}>Contact</MenuItem>
+          </Menu>
+        </Toolbar>
+>>>>>>> 86143f1... add hamburger button
       </AppBar>
     </div>
   );
