09c947c40e769fa47eeb6af083f248ab7a7b9373
Ben Galloway <bdgalloway85@gmail.com>
2021-03-07 17:05:01 -0600

    change Grid breakpoints

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,15 +16,15 @@ function About () {
         justify='center'
         alignItems='center'
       >
-        <Grid item xs={12} sm={12} md={4}>
-          <Box ml={2}>
+        <Grid item xs={12} sm={12} md={5}>
+          <Box ml={2} mt={2}>
             <img
               src={pic}
               alt='me'
             />
           </Box>
         </Grid>
-        <Grid item xs={12} sm={12} md={8}>
+        <Grid item xs={12} sm={12} md={7}>
           <Box mx='auto' p={1}>
             <p>
               My name is Ben. As a recent graduate of the University of Kansas Full-Stack coding bootcamp, I have new skills in JavaScript, React.js, Node.js and responsive web design. Being a self-starter and intellectual, I also enjoy learning new skills and collaborating in teams to maintain and create web applications. A bachelor’s degree in education and 5 years as a Systems Engineer give me a great foundation to build my skills upon. Working in education, healthcare and government give me inside background knowledge to create effective and user-first web applications. I enjoy writing code and employing new technologies to improve the efficiency, effectiveness and quality of the product and business.
