ae2b91ab2b32fc04a91bd468bf3c73400c8de7df
Ben Galloway <bdgalloway85@gmail.com>
2021-03-07 00:11:03 -0600

    add padding to contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 
 function Contact () {
@@ -10,20 +10,26 @@ function Contact () {
         </Box>
       </Typography>
       <hr />
-      <div className='row contact-icons'>
-        <div className='col text-center'>
-          <a href='https://www.linkedin.com/in/ben-g-b2a74353/'><i className='fab fa-linkedin fa-9x' />
-          </a>
-        </div>
-        <div className='col text-center'>
-          <a href='https://github.com/BenjDG'><i className='fab fa-github-square fa-9x' />
-          </a>
-        </div>
-        <div className='col text-center'>
-          <a href='mailto:bdgalloway85@gmail.com'><i className='fas fa-envelope fa-9x' />
-          </a>
-        </div>
-      </div>
+      <Grid container justify='space-evenly'>
+        <Box p={4}>
+          <Grid item>
+            <a href='https://www.linkedin.com/in/ben-g-b2a74353/'><i className='fab fa-linkedin fa-9x' />
+            </a>
+          </Grid>
+        </Box>
+        <Box p={4}>
+          <Grid item>
+            <a href='https://github.com/BenjDG'><i className='fab fa-github-square fa-9x' />
+            </a>
+          </Grid>
+        </Box>
+        <Box p={4}>
+          <Grid item>
+            <a href='mailto:bdgalloway85@gmail.com'><i className='fas fa-envelope fa-9x' />
+            </a>
+          </Grid>
+        </Box>
+      </Grid>
     </section>
   );
 }
