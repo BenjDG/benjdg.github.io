8453f925fe5644ef400c8bc0e1844253e9436d56
Ben Galloway <bdgalloway85@gmail.com>
2021-03-07 01:08:26 -0600

    replace anchor tags with Link

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import pic from './reducedSelf.png';
 
@@ -11,14 +11,27 @@ function About () {
         </Box>
       </Typography>
       <hr />
-      <img
-        src={pic}
-        className='img-fluid float-left p-4'
-        alt='me'
-      />
-      <p className='p-4'>
-        My name is Ben. As a recent graduate of the University of Kansas Full-Stack coding bootcamp, I have new skills in JavaScript, React.js, Node.js and responsive web design. Being a self-starter and intellectual, I also enjoy learning new skills and collaborating in teams to maintain and create web applications. A bachelor’s degree in education and 5 years as a Systems Engineer give me a great foundation to build my skills upon. Working in education, healthcare and government give me inside background knowledge to create effective and user-first web applications. I enjoy writing code and employing new technologies to improve the efficiency, effectiveness and quality of the product and business.
-      </p>
+      <Grid
+        container
+        justify='center'
+        alignItems='center'
+      >
+        <Grid item xs={12} sm={12} md={4}>
+          <Box ml={2}>
+            <img
+              src={pic}
+              alt='me'
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={12} md={8}>
+          <Box mx='auto' p={1}>
+            <p>
+              My name is Ben. As a recent graduate of the University of Kansas Full-Stack coding bootcamp, I have new skills in JavaScript, React.js, Node.js and responsive web design. Being a self-starter and intellectual, I also enjoy learning new skills and collaborating in teams to maintain and create web applications. A bachelor’s degree in education and 5 years as a Systems Engineer give me a great foundation to build my skills upon. Working in education, healthcare and government give me inside background knowledge to create effective and user-first web applications. I enjoy writing code and employing new technologies to improve the efficiency, effectiveness and quality of the product and business.
+            </p>
+          </Box>
+        </Grid>
+      </Grid>
     </section>
   );
 }
