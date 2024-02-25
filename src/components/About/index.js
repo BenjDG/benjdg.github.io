248cd0845f2c183e3157d263e45ba52d10337c5c
ben <bdgalloway85@gmail.com>
2024-02-25 08:37:54 -0600

    adjust css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,9 +17,9 @@ function About () {
         alignItems='center'
       >
         <Grid item xs={12} sm={12} md={5}>
-          <Box m='auto'>
+          <Box m='auto' p={1}>
             <img
-              style={{ height: '20rem', margin: '2rem', display: 'block' }}
+              style={{ height: '20rem', marginLeft: 'auto', marginRight:'auto', display: 'block' }}
               src={pic}
               alt='me'
             />
@@ -27,7 +27,7 @@ function About () {
         </Grid>
         <Grid item xs={12} sm={12} md={7}>
           <Box mx='auto' p={1}>
-            <p style={{textIndent: '30px'}}>
+            <p style={{textIndent: '30px', textAlign: 'justify'}}>
             Software Developer that loves to code and learn. A proactive self-starter who thrives on continuous learning and relishes the challenge of mastering new skills. My journey in programming has led me to become proficient in a range of languages including Python, PL/SQL, SQL,Node.js, React.js, and JavaScript. I'm also comfortable with Linux, Git, Github, and CI/CD pipelines. I also enjoy working with all kinds of databases including Oracle, SQL Server, MySQL, MongoDB, and others. I also have experience working with Postman, REST, and other API technologies. I'm also knowledgable in the software lifecycle development process, as well as Agile and working with Jira. Over 8 years of technology experience from healthcare, higher education and government. I also enjoy working in a team and collaborating with other developers.  
             </p>
           </Box>
